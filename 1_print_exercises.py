b12576595b608c082a047fcbafdb2cdfaac2e626
getdoorimed <geomon00@kingshigh.school.nz>
2022-02-25 12:25:17 +1300

    Update 1_print_exercises.py

--- 1_print_exercises.py
+++ 1_print_exercises.py
@@ -1,3 +1,5 @@
+#4.1 Print Exercises
+
 print ("George Montalban")
 print ("Mrs. White")
 print ("King's High School")
@@ -9,3 +11,32 @@ print (658/7)
 print (2022 - 1969)
 print ("ABA")
 print ("DTE")
+print ("Hello World" * 30)
+
+
+#Things that don't work by themselves 
+#str(55+65)
+#int(55.0)
+#float(44+10)
+#To make this work, you would have to use "print (float())"
+
+print (float(54/66))
+print (str(55+65))
+print (float(55.43))
+
+print ("Super idol de shiao rong dou\n mei ni de tian\n Ba yue jheng wu de yang guang\n dou mei ni yao yan\n Re ai yi bai ling wu du de ni\n Didi ching chun de chuen liu shui")
+print ("\rHe is a man of Focus, Commitment, and Sheer Frekkin Will\r")
+
+print ("A string is a collection of Alphabetical letters, words and other characters")
+print ("An integer is a number line")
+print ("A float is just like an integer but instead it includes the decimals")
+
+print (float(543/3))
+print (543%3)
+
+print (1000%9)
+print (float(434/10))
+print (3%2)
+print (5%5)
+print (float(555/2))
+print (float(123/2))
