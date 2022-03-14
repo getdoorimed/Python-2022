4b0c64fa1c3bb728a0b994a3150d36b0ff1d2c7a
getdoorimed <geomon00@kingshigh.school.nz>
2022-03-15 12:24:52 +1300

    Update 1_print_exercises.py

--- 1_print_exercises.py
+++ 1_print_exercises.py
@@ -20,23 +20,47 @@ print ("Hello World" * 30)
 #float(44+10)
 #To make this work, you would have to use "print (float())"
 
+
 print (float(54/66))
 print (str(55+65))
 print (float(55.43))
 
+
+#4.4 Excercises 
 print ("Super idol de shiao rong dou\n mei ni de tian\n Ba yue jheng wu de yang guang\n dou mei ni yao yan\n Re ai yi bai ling wu du de ni\n Didi ching chun de chuen liu shui")
 print ("\rHe is a man of Focus, Commitment, and Sheer Frekkin Will\r")
-
 print ("A string is a collection of Alphabetical letters, words and other characters")
 print ("An integer is a number line")
 print ("A float is just like an integer but instead it includes the decimals")
-
 print (float(543/3))
 print (543%3)
+
 
 print (1000%9)
 print (float(434/10))
 print (3%2)
 print (5%5)
-print (float(555/2))
-print (float(123/2))
+print (str(555/2))
+print (str(123/2))
+print (str(123//2))
+print (str(764%2))
+print (int(165//2))
+
+
+print (float(2+2))
+print (int(12+12))
+print (int(32+41))
+
+#skip funny haha
+
+#5 Variables
+
+player_lives = int(input("How many lives would a player have in this game"))
+print ("The amount of lives you chose to pick is",player_lives)
+user_login = input("Please input a login name")
+money_storage = int(input("Please input how much money you would like to store"))
+
+
+
+
+
