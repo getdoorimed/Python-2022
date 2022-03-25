f836b62e48bfe48702457b3662bf3aabeb148469
getdoorimed <geomon00@kingshigh.school.nz>
2022-03-25 13:23:24 +1300

    Work 25032022

--- game1.py
+++ game1.py
@@ -1,25 +1,7 @@
 from adventurelib import *
-
-
-@when("ben")
-def ben():
-	say("Ben: Yeees")
-
-@when("ben do you love god")
-def ben_do_you_love_god():
-	say("Ben: No.")
-
-@when("ben shut your ass up")
-def benshutyoahhup():
-	say("Ben: Hohoho.")
-@when("comb hair")
-@when("comb")
-def comb_hair():
-	say("""
-		You brush your long flowing locks with
-		the gold hairbrush that you have selected from the
-		in the red basket.
-		""")
+Room.items = Bag()
+"Hi"
+#define rooms
 
 space = Room("""
 	You are drifting in space. It feels very cold.
@@ -46,6 +28,43 @@ mess_hall = Room("A very messy hall.")
 
 escape_pods = Room("A room that people who have no guts go to")
 
+#connections
+
+spaceship.east = hallway
+spaceship.south = quarters
+hallway.east = bridge
+hallway.north = cargo
+hallway.south = mess_hall
+hallway.west = spaceship
+cargo.east = docking
+bridge.south = escape_pods
+mess_hall.west = quarters
+
+#define variables
+current_room = space
+inventory = Bag()
+
+
+#define items
+Item.description = "" #this adds a blank description to each item
+
+knife = Item("a dirty knife","knife")
+knife.description = "the knife has a dull sheen to it but it looks rather sharp."
+
+red_keycard = Item("a red keycard","keycard","red card","red card")
+red_keycard.description = "It's a red keycard. It probably opens a door or a locker."
+
+gun = Item("gun")
+gun.description = "A rusty but handy handgun."
+
+doll = Item("doll")
+doll.description = "A menacing doll, i wonder what it does."
+
+
+
+#adds items to bags
+
+
 
 @when("enter airlock")
 @when("enter spaceship")
@@ -57,22 +76,33 @@ def enter_spaceship():
 		say("There is no airlock here")
 		return
 	else:
-		current_room = enter_spaceship
-	print("""You heave yourself into the spaceship and slam you hand on the button to close the door.""")
+		current_room = spaceship
+		print("""You heave yourself into the spaceship and slam you hand on the button to close the door.""")
+		print(current_room)
+
+
+
+@when("look")
+def look():
 	print(current_room)
+	print(f"There are exits to the {current_room.exits()}.")
+	if len(current_room.items) > 0: #if there are some items in the room
+		print("You also see: ")
+		for item in current_room.items:
+			print(item)#print out each item
+
+@when("get ITEM")
+@when("take ITEM")
+@when("pick up ITEM")
+def pickup(item):
+	if item in current_room.items:
+		t = current_room.items.take(item)
+		inventory.add(t)
+		print(f"You pick up the {item}")
+	else:
+		print(f"You don't see a {item}")
 
 
-#variables
-current_room = space
-spaceship.east = hallway
-spaceship.south = quarters
-hallway.east == bridge
-hallway.north == cargo
-hallway.south == mess_hall
-hallway.west == spaceship
-cargo.east == docking
-bridge.south == escape_pods
-mess_hall.west == quarters
 
 @when ("go DIRECTION")
 def travel(direction):
@@ -82,6 +112,22 @@ def travel(direction):
 		print(f"You go {direction}.")
 		print(current_room)
 	
+@when("inventory")
+@when("show inventory")
+@when("what is in my pocket")
+def player_inventory():
+	print("You are carrying")
+	for item in inventory:
+		print(item)
+
+@when("look at ITEM")
+def look_at(item):
+	if item in inventory:
+		t = inventory.find(item)
+		print(t.description)
+	else:
+		print(f"You aren't carrying an {item}")
+
 
 
 def main():
